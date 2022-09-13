c68d03228e7afbc73fd9a92fb165a41ff65a95c0
jugui93 <juanguillermo93@gmail.com>
2022-09-12 20:39:01 -0500

    proyecto mercadoliebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, "./public");
+const app = express();
+
+app.use( express.static(publicPath));
+app.set('puerto', process.env.PORT || 3001);
+
+app.listen(app.get('puerto'), () => 
+console.log(`Servidor corriendo en el puerto 3000 ${app.get('puerto')}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
